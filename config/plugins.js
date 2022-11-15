fa7dc4f8b6191324a9fccc53b575b176f48bd425
trucnguyen <truc.nguyen@akadigital.vn>
2022-11-15 11:02:08 +0700

    2 commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
-    zalo: {
+    'zalo': {
         messageRules: ['#', "*", "?"],
         url: "https://openapi.zalo.me/v2.0/oa",
     },
 
-    fbm: {
+    'fbm': {
         loginURL: "https://portal-uat.prudential.com.vn/pulse-lead-corporate/auth/login",
         sendURL: "https://portal-uat.prudential.com.vn/pulse-lead-corporate/message/send-msg",
         username: "pulseleaduat",
         password: "Prudential01@"
     },
 
-    netcore: {
+    'netcore': {
         enabled: true,
         resolve: '.src/plugins/netcore'
     }
