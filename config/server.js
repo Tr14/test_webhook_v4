24fbbedc9c569a84d960a8af86d1e39e05ba9805
trucnguyen <truc.nguyen@akadigital.vn>
2022-11-15 10:57:18 +0700

    2 commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,14 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '66d343acd9de7727c33374ad339af7dd'),
+    },
+  },
+  redis: {
+    port: 6379,
+    host: '127.0.0.1',
+    password: '!Vola@2021@'
   },
 });
