8f269c9e8e7ccd770a5a1c4196ad49cc5723ab30
trucnguyen <truc.nguyen@akadigital.vn>
2022-11-15 16:51:45 +0700

    2 commit

--- config/plugins.js
+++ config/plugins.js
@@ -7,7 +7,7 @@ module.exports = {
     */
 
     'fbm': {
-        enable: true,
+        enabled: true,
         resolve: './src/plugins/fbm',
         loginURL: "https://portal-uat.prudential.com.vn/pulse-lead-corporate/auth/login",
         sendURL: "https://portal-uat.prudential.com.vn/pulse-lead-corporate/message/send-msg",
