0a511f411857e821bfddbabc50102be2669078fd
trucnguyen <truc.nguyen@akadigital.vn>
2022-11-15 11:58:37 +0700

    2 commit

--- config/database.js
+++ config/database.js
@@ -3,11 +3,10 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connector: 'bookshelf',
     connection: {
-      client: 'postgres',
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
-      username: env('DATABASE_USERNAME', 'truc'),
+      user: env('DATABASE_USERNAME', 'truc'),
       password: env('DATABASE_PASSWORD', 'xuantruc1499'),
       ssl: env.bool('DATABASE_SSL', false),
     }
