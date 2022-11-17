377337daaae3ae5065beb34a6bbdfa582e2b04b2
Tr14 <xuantrucx111@gmail.com>
2022-11-17 23:13:24 +0700

    3 commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "https://aka-webhook.onrender.com"),
+  url: "https://aka-webhook.onrender.com",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '66d343acd9de7727c33374ad339af7dd'),
