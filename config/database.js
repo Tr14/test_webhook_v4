5acd51623be974ebdf06b8537e503c707f3c92c6
Tr14 <xuantrucx111@gmail.com>
2022-11-17 22:53:30 +0700

    3 commit

--- config/database.js
+++ config/database.js
@@ -1,25 +1,20 @@
 const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  // const { host, port, database, user, password, ssl } = parse("postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a.oregon-postgres.render.com/postgres_sfxp?ssl=true");
+module.exports = ({ env }) => {
+  const { host, port, database, user, password, ssl } = parse("postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a.oregon-postgres.render.com/postgres_sfxp?ssl=true");
 
-  // return {
-  //   connection: {
-  //     client: "postgres",
-  //     connection: {
-  //       host,
-  //       port,
-  //       database,
-  //       user,
-  //       password,
-  //       ssl
-  //     },
-  //     debug: false,
-  //   },
-  // };
-
-  url: "postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a.oregon-postgres.render.com/postgres_sfxp?ssl=true",
-  dirs: {
-    public: "/data/public"
-  }
-});
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl
+      },
+      debug: false,
+    },
+  };
+};
