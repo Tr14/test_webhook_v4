5f0fec2fbf6c9b9bf859cf4e3f9381c7d3f15553
Tr14 <xuantrucx111@gmail.com>
2022-11-17 22:19:42 +0700

    3 commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse("postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a/postgres_sfxp");
+  const { host, port, database, user, password } = parse("postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a.oregon-postgres.render.com/postgres_sfxp");
 
   return {
     connection: {
