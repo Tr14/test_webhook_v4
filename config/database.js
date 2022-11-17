3815c280602bc5803b6d4ce2feaa6e8f69f55113
Tr14 <xuantrucx111@gmail.com>
2022-11-17 21:52:31 +0700

    3 commit

--- config/database.js
+++ config/database.js
@@ -3,8 +3,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connector: 'bookshelf',
     connection: {
-      host: env('DATABASE_HOST', 'postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a/postgres_sfxp'),
-      password: env('DATABASE_PASSWORD', 'Wy22xIVR3Y2WJUr184hDDd5fMYRgefED'),
+      host: env('DATABASE_HOST', 'postgres://tructest:Wy22xIVR3Y2WJUr184hDDd5fMYRgefED@dpg-cdr4a6en6mpqj2ddu2b0-a/postgres_sfxp')
     }
   }
 });
