e36c198cc53ce6d81e33026bc83ee89eb8d37ffb
trucnguyen <truc.nguyen@akadigital.vn>
2023-03-02 18:16:57 +0700

    3/2/2023 5th time

--- src/plugins/firebasetoken/server/controllers/firebasetoken.js
+++ src/plugins/firebasetoken/server/controllers/firebasetoken.js
@@ -9,29 +9,29 @@ module.exports = ({ strapi }) => ({
     console.log(data_body)
 
     let token = [];
-    let deviceID = [];
-    let deviceName = [];
-    let deviceOS = [];
-    let platform = [];
-    let status = [];
+    //let deviceID = [];
+    //let deviceName = [];
+    //let deviceOS = [];
+    //let platform = [];
+    //let status = [];
 
     for (let i = 0; i < data_body.length; i++) {
       token = data_body[i].Token;
-      deviceID = data_body[i].DeviceID;
-      deviceName = data_body[i].DeviceName;
-      deviceOS = data_body[i].DeviceOS;
-      platform = data_body[i].Platform;
-      status = data_body[i].Status;
+      //deviceID = data_body[i].DeviceID;
+      //deviceName = data_body[i].DeviceName;
+      //deviceOS = data_body[i].DeviceOS;
+      //platform = data_body[i].Platform;
+      //status = data_body[i].Status;
 
       let entry = await strapi.db.query('plugin::firebasetoken.firebasetoken').create({
         data: {
-          record: "AKA",
+          //record: "AKA",
           token: token,
-          deviceID: deviceID,
-          deviceName: deviceName,
-          deviceOS: deviceOS,
-          platform: platform,
-          status: status
+          //deviceID: deviceID,
+          //deviceName: deviceName,
+          //deviceOS: deviceOS,
+          //platform: platform,
+          //status: status
         }
       });
     };
