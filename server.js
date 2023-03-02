d217ed49d767af0fc338f67d5f92665d38d23009
trucnguyen <truc.nguyen@akadigital.vn>
2023-03-02 13:49:44 +0700

    3/2/2023 3rd time

--- server.js
+++ server.js
@@ -7,53 +7,23 @@ const chokidar = require('chokidar');
 const execa = require('execa');
 
 const { logger } = require('strapi-utils');
-const loadConfiguration = require('strapi/lib/core/app-configuration');
-const strapi = require('strapi');
+const loadConfiguration = require('../core/app-configuration');
+const strapi = require('../index');
 
 /**
  * `$ strapi develop`
  *
  */
-//COPY strapi TO NODE MODULES
-// let strapiadminsrc = path.resolve('./src/', 'strapi-admin');
-// let strapiadminnms = path.resolve('./node_modules', 'strapi-admin');
-
-// fs.removesync(strapiadminnms);
-// fs.copysync(strapiadminsrc, strapiadminnms);
-// console.log('\ncopy admin to node_modules successfully.\n');
-
-let strapisrc = path.resolve('./src/strapi', 'lib');
-let strapinms = path.resolve('./node_modules/strapi', 'lib');
-
-fs.removeSync(strapinms);
-fs.copySync(strapisrc, strapinms);
-console.log('\ncopy core to node_modules successfully.\n');
-
-// let strapidatabase = path.resolve('./src/strapi-database', 'lib');
-// let strapidatabasenms = path.resolve('./node_modules/strapi-database', 'lib');
-
-// fs.removesync(strapidatabasenms);
-// fs.copysync(strapidatabase, strapidatabasenms);
-// console.log('\ncopy database to node_modules successfully.\n');
-
-// let strapiconnector = path.resolve('./src/strapi-connector-mongoose', 'lib');
-// let strapiconnectornms = path.resolve('./node_modules/strapi-connector-mongoose', 'lib');
-
-// fs.removesync(strapiconnectornms);
-// fs.copysync(strapiconnector, strapiconnectornms);
-// console.log('\ncopy connector mongoose to node_modules successfully.\n');
-
-
-	let build = false;
-	let watchAdmin = false;
-	
+module.exports = async function({ build, watchAdmin, polling, browser }) {
   const dir = process.cwd();
   const config = loadConfiguration(dir);
 
   const adminWatchIgnoreFiles = config.get('server.admin.watchIgnoreFiles', []);
+  const serveAdminPanel = config.get('server.admin.serveAdminPanel', true);
 
+  const buildExists = fs.existsSync(path.join(dir, 'build'));
   // Don't run the build process if the admin is in watch mode
-  if (build && !watchAdmin && !fs.existsSync(path.join(dir, 'build'))) {
+  if (build && !watchAdmin && serveAdminPanel && !buildExists) {
     try {
       execa.shellSync('npm run -s build -- --no-optimization', {
         stdio: 'inherit',
@@ -67,7 +37,7 @@ console.log('\ncopy core to node_modules successfully.\n');
     if (cluster.isMaster) {
       if (watchAdmin) {
         try {
-          execa('npm', ['run', '-s', 'strapi', 'watch-admin'], {
+          execa('npm', ['run', '-s', 'strapi', 'watch-admin', '--', '--browser', browser], {
             stdio: 'inherit',
           });
         } catch (err) {
@@ -103,11 +73,12 @@ console.log('\ncopy core to node_modules successfully.\n');
         serveAdminPanel: watchAdmin ? false : true,
       });
 
-      // watchFileChanges({
-      //   dir,
-      //   strapiInstance,
-      //   watchIgnoreFiles: adminWatchIgnoreFiles,
-      // });
+      watchFileChanges({
+        dir,
+        strapiInstance,
+        watchIgnoreFiles: adminWatchIgnoreFiles,
+        polling,
+      });
 
       process.on('message', message => {
         switch (message) {
@@ -127,6 +98,7 @@ console.log('\ncopy core to node_modules successfully.\n');
     logger.error(e);
     process.exit(1);
   }
+};
 
 /**
  * Init file watching to auto restart strapi app
@@ -135,7 +107,7 @@ console.log('\ncopy core to node_modules successfully.\n');
  * @param {Strapi} options.strapi - Strapi instance
  * @param {array} options.watchIgnoreFiles - Array of custom file paths that should not be watched
  */
-function watchFileChanges({ dir, strapiInstance, watchIgnoreFiles }) {
+function watchFileChanges({ dir, strapiInstance, watchIgnoreFiles, polling }) {
   const restart = () => {
     if (strapiInstance.reload.isWatching && !strapiInstance.reload.isReloading) {
       strapiInstance.reload.isReloading = true;
@@ -145,6 +117,7 @@ function watchFileChanges({ dir, strapiInstance, watchIgnoreFiles }) {
 
   const watcher = chokidar.watch(dir, {
     ignoreInitial: true,
+    usePolling: polling,
     ignored: [
       /(^|[/\\])\../, // dot files
       /tmp/,
@@ -160,8 +133,6 @@ function watchFileChanges({ dir, strapiInstance, watchIgnoreFiles }) {
       '**/index.html',
       '**/public',
       '**/public/**',
-      '**/cypress',
-      '**/cypress/**',
       '**/*.db*',
       '**/exports/**',
       ...watchIgnoreFiles,
