b6536bbdc8dcd7b662f8989d859bcc8f39ce3dbc
trucnguyen <truc.nguyen@akadigital.vn>
2023-03-02 15:28:52 +0700

    3/2/2023 3rd time

--- src/plugins/netcore/server/content-types/index.js
+++ src/plugins/netcore/server/content-types/index.js
@@ -2,11 +2,9 @@
 const facebooklead = require('./facebooklead');
 const netcorelead = require('./netcorelead');
 const netcorelog = require('./netcorelog');
-const firebaseToken = require('./firebasetoken')
 
 module.exports = {
     facebooklead,
     netcorelead,
-    netcorelog,
-    firebasetoken
+    netcorelog
 };
