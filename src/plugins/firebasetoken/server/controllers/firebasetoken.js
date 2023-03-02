9aef8cdba094bc5943359b5c89c8fc30241083dc
trucnguyen <truc.nguyen@akadigital.vn>
2023-03-02 16:10:00 +0700

    3/2/2023 5th time

--- src/plugins/firebasetoken/server/controllers/firebasetoken.js
+++ src/plugins/firebasetoken/server/controllers/firebasetoken.js
@@ -1,11 +1,28 @@
 'use strict';
 
 module.exports = ({ strapi }) => ({
-  sendToken(ctx) {
-    ctx.body = "SEND TOKEN"
+  async sendToken(ctx) {
+    let token = [];
+    let deviceID = [];
+    let deviceName = [];
+    let deviceOS = [];
+    let platform = [];
+    let status = [];
+
+    let entry = await strapi.db.query('plugin::firebasetoken.firebasetoken').create({
+      data: {
+        record: "AKA",
+        token: token,
+        deviceID: deviceID,
+        deviceName: deviceName,
+        deviceOS: deviceOS,
+        platform: platform,
+        status: status
+      }
+    });
   },
 
-  getToken(ctx) {
+  async getToken(ctx) {
     ctx.body = "GET TOKEN"
   },
 });
