82be97275639a5b471791928d37a62786ac17864
trucnguyen <truc.nguyen@akadigital.vn>
2023-03-02 18:03:06 +0700

    3/2/2023 5th time

--- src/plugins/firebasetoken/server/controllers/firebasetoken.js
+++ src/plugins/firebasetoken/server/controllers/firebasetoken.js
@@ -4,7 +4,7 @@ module.exports = ({ strapi }) => ({
   async sendToken(ctx) {
     ctx.body = "AKADIGITAL"
 
-    let data_body = ctx.request.body;
+    let data_body = ctx.request.body.data;
 
     console.log(data_body)
 
