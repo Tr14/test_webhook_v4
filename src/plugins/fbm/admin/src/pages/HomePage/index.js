2c3916fe18969fbefcdc530469e0cc5f418b314f
Nguyễn Trúc <xuantrucx111@gmail.com>
2023-08-28 15:49:12 +0700

    add facebook webhook

--- src/plugins/fbm/admin/src/pages/HomePage/index.js
+++ src/plugins/fbm/admin/src/pages/HomePage/index.js
@@ -11,8 +11,8 @@ import pluginId from '../../pluginId';
 const HomePage = () => {
   return (
     <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+      <h1 style={{ color: "white" }}>Welcome to Facebook section</h1>
+      <button style={{ color: "blue", background: "white" }} href="https://www.facebook.com/v17.0/dialog/oauth?client_id=843916146887327&redirect_uri=https://dev.akadigital.net/&scope=ads_management">Approve Facebook Permission</button>
     </div>
   );
 };
